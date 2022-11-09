10d9d80de24c6a701d9337171e84847242a1f738
Jordan <jdb010@lvc.edu>
2022-11-09 12:42:39 -0500

    outline

---
+++ Script.js
@@ -0,0 +1,26 @@
+/* Most of the question logic and switching between questions
+   ... probably a changeQuestion function and checkAnswer function*/
+
+/* what should the punishment be for an incorrect question? */
+
+/* we can also mess around with different audio clips throughout 
+   and maybe a video clip at the end (voldemort killing harry for a loss
+   and harry killing voldemort for a win) */
+
+/* keep track of the questions we have already shown to not duplicate */
+function changeQuestion() {
+
+}
+
+/* store the answers to each question somehow - not sure how data structures work
+   in js, but a hashMap / dictionary would work well 
+   We could then pass in the question number to check it. */
+function checkAnswer(question) {
+
+}
+
+/* click functionality in here? */
+$(document).ready(function() {
+
+
+}); // end document.ready()
